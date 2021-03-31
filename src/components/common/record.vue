f4925e1d4359aa4ee6ce9d9b5ae0da35807acfd6
jsonwu5 <jsonwu@qq.com>
2021-03-31 17:41:14 +0800

    新增自定义列组件

---
+++ src/components/common/record.vue
@@ -0,0 +1,177 @@
+<template>
+  <a-modal
+    title="加减仓详情"
+    wrapClassName="record"
+    :visible="visible"
+    @cancel="close()"
+    :footer="null"
+    width="calc(100% - 30px)"
+  >
+    <div class="width-100">
+      <a-button class="mb5" @click="columnsVisible = true">自定义列</a-button>
+      <a-table
+        :pagination="false"
+        :columns="selectedColumns"
+        :dataSource="list"
+        :rowKey="(record, index) => index"
+        :scroll="{ y: 300 }"
+        bordered
+        centered
+      >
+        <template slot-scope="text, row, index" slot="index">
+          <span>{{ index + 1 }}</span>
+        </template>
+        <span slot="created" slot-scope="created">{{
+          created | formatDate
+        }}</span>
+      </a-table>
+
+      <custom-columns
+        v-if="columnsVisible"
+        v-model="selectedColumns"
+        :visible.sync="columnsVisible"
+        :columns.sync="columns"
+      ></custom-columns>
+    </div>
+  </a-modal>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import CustomColumns from "@components/common/customColumns";
+
+export default {
+  name: "record",
+  components: { CustomColumns },
+  model: {
+    prop: "visible",
+    event: "update"
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    coin: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      columnsVisible: false,
+      selectedColumns: [],
+      columns: [
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "序号",
+          align: "center",
+          dataIndex: "index",
+          width: 60,
+          // fixed: "left",
+          scopedSlots: { customRender: "index" }
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "时间",
+          align: "center",
+          ellipsis: true,
+          // width: 50,
+          dataIndex: "created"
+        },
+        {
+          checked: false, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "交易类型",
+          align: "center",
+          ellipsis: true,
+          width: 100,
+          dataIndex: "type"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "交易对",
+          align: "center",
+          ellipsis: true,
+          width: 75,
+          dataIndex: "symbol"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "方向",
+          align: "center",
+          ellipsis: true,
+          width: 50,
+          dataIndex: "role"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "价格",
+          align: "center",
+          ellipsis: true,
+          width: 100,
+          dataIndex: "price"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "数量",
+          align: "center",
+          ellipsis: true,
+          width: 100,
+          dataIndex: "realAmount"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "成交额",
+          align: "center",
+          ellipsis: true,
+          // width: 100,
+          dataIndex: "realVolume"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "手续费",
+          align: "center",
+          ellipsis: true,
+          // width: 100,
+          dataIndex: "points"
+        }
+      ]
+    };
+  },
+  computed: {
+    ...mapState(["buySellRecords"]),
+    list() {
+      return this.buySellRecords.filter(item => item.coin === this.coin);
+    }
+  },
+  created() {
+    this.selectedColumns = this.columns.filter(item => item.checked);
+  },
+  methods: {
+    close() {
+      this.$emit("update", false);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.record {
+  .ant-modal-body {
+    padding: 5px;
+  }
+  .ant-table-thead > tr > th,
+  .ant-table-tbody > tr > td {
+    padding: 2px !important;
+  }
+}
+</style>
