894b1eea3e3646cc586a56de5d3549f681bca0cb
Jason <jsonwu@qq.com>
2021-04-23 20:37:50 +0800

    调整和优化在独立窗口中打开

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,26 +1,39 @@
 <template>
   <a-config-provider :locale="zhCN">
     <div class="app">
-      <a-tabs v-model="activeKey">
-        <a-tab-pane key="market">
-          <span slot="tab">
-            <a-icon type="sliders" />
-            实时行情
-          </span>
-        </a-tab-pane>
-        <a-tab-pane key="earnings">
-          <span slot="tab">
-            <a-icon type="bar-chart" />
-            收益分析
-          </span>
-        </a-tab-pane>
-        <a-tab-pane key="setting">
-          <span slot="tab">
-            <a-icon type="setting" />
-            设置
-          </span>
-        </a-tab-pane>
-      </a-tabs>
+      <div class="flex jsb ac tabs pr5">
+        <a-tabs v-model="activeKey" class="flex-1">
+          <a-tab-pane key="market">
+            <span slot="tab">
+              <a-icon type="sliders" />
+              实时行情
+            </span>
+          </a-tab-pane>
+          <a-tab-pane key="earnings">
+            <span slot="tab">
+              <a-icon type="bar-chart" />
+              收益分析
+            </span>
+          </a-tab-pane>
+          <a-tab-pane key="setting">
+            <span slot="tab">
+              <a-icon type="setting" />
+              设置
+            </span>
+          </a-tab-pane>
+        </a-tabs>
+        <a-dropdown>
+          <a-menu slot="overlay" @click="openInTab">
+            <a-menu-item key="1" v-if="openType !== 1">
+              以浏览器标签页打开
+            </a-menu-item>
+            <a-menu-item key="2" v-if="openType !== 2">
+              以独立窗口打开
+            </a-menu-item>
+          </a-menu>
+          <a-button shape="circle" icon="down"></a-button>
+        </a-dropdown>
+      </div>
 
       <market v-if="activeKey === 'market'" />
       <earnings v-if="activeKey === 'earnings'"></earnings>
@@ -36,6 +49,7 @@ import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
 import { getStorage } from "@/tools/storage.js";
 import Earnings from "@components/earnings";
 import Setting from "@components/setting";
+import { getQueryString } from "@tools";
 
 export default {
   name: "App",
@@ -47,10 +61,14 @@ export default {
     };
   },
   computed: {
-    ...mapState(["manifest", "userLang"]),
+    ...mapState(["userLang", "openType"]),
     ...mapGetters(["i18n"])
   },
   created() {
+    let openType = getQueryString("openType");
+    openType = Number(openType);
+    this._setOpenType(openType);
+
     getStorage("userLang").then(res => {
       this._setUserLang(res.userLang);
       if (!this.userLang) {
@@ -65,16 +83,47 @@ export default {
   },
   methods: {
     ...mapActions(["_getManifest", "_getLanguageAll"]),
-    ...mapMutations(["_setUserLang"])
+    ...mapMutations(["_setUserLang", "_setOpenType"]),
+    openInTab(e) {
+      // 以浏览器标签页打开
+      if (e.key === "1") {
+        chrome.tabs.create(
+          {
+            url: `${chrome.runtime.getURL("popup.html")}?openType=${e.key}`
+          },
+          e => {
+            console.log(e);
+          }
+        );
+      } else {
+        // 以独立窗口打开
+        chrome.windows.create(
+          {
+            url: `${chrome.runtime.getURL("popup.html")}?openType=${e.key}`,
+            width: 830,
+            height: 550,
+            top: 200,
+            type: "popup"
+          },
+          e => {
+            console.log(e);
+          }
+        );
+      }
+    }
   }
 };
 </script>
 
-<style lang="less">
+<style scoped lang="less">
 .app {
   transition: all 0.3s;
-  .ant-tabs-bar {
+  .tabs {
+    border-bottom: 1px solid #e8e8e8;
+  }
+  /deep/ .ant-tabs-bar {
     margin: 0;
+    border-bottom: none;
   }
 }
 </style>
