a490eb6386ae2020439f3fbc84bb6df3fa00b202
Jason <jsonwu@qq.com>
2021-04-24 21:44:09 +0800

    加减仓详情优化

--- src/components/common/record.vue
+++ src/components/common/record.vue
@@ -3,18 +3,24 @@
     title="加减仓详情"
     wrapClassName="record"
     :visible="visible"
+    centered
     @cancel="close()"
     :footer="null"
     width="calc(100% - 30px)"
   >
-    <div class="width-100">
-      <custom-columns v-model="selectedColumns" :columns="columns"
-        ><a-button class="mb5">自定义列</a-button></custom-columns
-      >
+    <div class="width-100 pb15 pl15 pr15">
+      <custom-columns
+        class="mt15 mb15"
+        v-model="selectedColumns"
+        :columns="columns"
+        placement="rightTop"
+        :checkedList.sync="checkedList"
+      ></custom-columns>
+
       <a-table
         :pagination="false"
         :columns="selectedColumns"
-        :dataSource="list"
+        :dataSource="data"
         :rowKey="(record, index) => index"
         :scroll="{ y: 300 }"
         bordered
@@ -26,13 +32,42 @@
         <span slot="created" slot-scope="created">{{
           created | formatDate
         }}</span>
+        <a-space slot="action" align="center" slot-scope="value, row">
+          <a-popconfirm
+            placement="left"
+            title="确认要删除该条记录吗"
+            :ok-text="i18n.affirm || '确认'"
+            :cancel-text="i18n.cancel || '取消'"
+            @confirm="deleteRecord(row)"
+          >
+            <a-tooltip
+              placement="left"
+              :get-popup-container="e => e.parentElement"
+            >
+              <template slot="title">
+                删除该条记录
+              </template>
+              <a-icon class="pointer f18" type="delete" theme="filled" />
+            </a-tooltip>
+          </a-popconfirm>
+
+          <a-tooltip
+            placement="left"
+            :get-popup-container="e => e.parentElement"
+          >
+            <template slot="title">
+              修改
+            </template>
+            <a-icon class="pointer f18" type="edit" />
+          </a-tooltip>
+        </a-space>
       </a-table>
     </div>
   </a-modal>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapGetters, mapState, mapMutations } from "vuex";
 import CustomColumns from "@components/common/customColumns";
 
 export default {
@@ -56,6 +91,7 @@ export default {
     return {
       columnsVisible: false,
       selectedColumns: [],
+      checkedList: [],
       columns: [
         {
           checked: true, // 是否默认勾选
@@ -65,16 +101,8 @@ export default {
           dataIndex: "index",
           width: 60,
           // fixed: "left",
-          scopedSlots: { customRender: "index" }
-        },
-        {
-          checked: true, // 是否默认勾选
-          checkDisabled: false, // 是否默认禁用
-          title: "时间",
-          align: "center",
-          ellipsis: true,
-          // width: 50,
-          dataIndex: "created"
+          scopedSlots: { customRender: "index" },
+          i18nKey: "colIndex"
         },
         {
           checked: false, // 是否默认勾选
@@ -83,7 +111,8 @@ export default {
           align: "center",
           ellipsis: true,
           width: 100,
-          dataIndex: "type"
+          dataIndex: "type",
+          i18nKey: "colType"
         },
         {
           checked: true, // 是否默认勾选
@@ -92,7 +121,8 @@ export default {
           align: "center",
           ellipsis: true,
           width: 75,
-          dataIndex: "symbol"
+          dataIndex: "symbol",
+          i18nKey: "colSymbol"
         },
         {
           checked: true, // 是否默认勾选
@@ -101,7 +131,8 @@ export default {
           align: "center",
           ellipsis: true,
           width: 50,
-          dataIndex: "role"
+          dataIndex: "role",
+          i18nKey: "colRole"
         },
         {
           checked: true, // 是否默认勾选
@@ -110,7 +141,8 @@ export default {
           align: "center",
           ellipsis: true,
           width: 100,
-          dataIndex: "price"
+          dataIndex: "price",
+          i18nKey: "colPrice"
         },
         {
           checked: true, // 是否默认勾选
@@ -119,7 +151,8 @@ export default {
           align: "center",
           ellipsis: true,
           width: 100,
-          dataIndex: "realAmount"
+          dataIndex: "realAmount",
+          i18nKey: "colRealAmount"
         },
         {
           checked: true, // 是否默认勾选
@@ -128,7 +161,8 @@ export default {
           align: "center",
           ellipsis: true,
           // width: 100,
-          dataIndex: "realVolume"
+          dataIndex: "realVolume",
+          i18nKey: "colRealVolume"
         },
         {
           checked: true, // 是否默认勾选
@@ -137,23 +171,56 @@ export default {
           align: "center",
           ellipsis: true,
           // width: 100,
-          dataIndex: "points"
+          dataIndex: "points",
+          i18nKey: "colPoints"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: false, // 是否默认禁用
+          title: "时间",
+          align: "center",
+          ellipsis: true,
+          // width: 50,
+          dataIndex: "created",
+          i18nKey: "colTime"
+        },
+        {
+          checked: true, // 是否默认勾选
+          checkDisabled: true, // 是否默认禁用
+          title: "操作",
+          align: "center",
+          dataIndex: "action",
+          scopedSlots: { customRender: "action" },
+          i18nKey: "colOperation"
         }
-      ]
+      ],
+      data: []
     };
   },
   computed: {
     ...mapState(["buySellRecords"]),
+    ...mapGetters(["i18n"]),
+    records() {
+      return this.buySellRecords.map((item, index) => ({ ...item, index }));
+    },
     list() {
-      return this.buySellRecords.filter(item => item.coin === this.coin);
+      return this.records.filter(item => item.coin === this.coin);
     }
   },
   created() {
+    this.data = JSON.parse(JSON.stringify(this.list));
     this.selectedColumns = this.columns.filter(item => item.checked);
   },
   methods: {
+    ...mapMutations(["_setBuySellRecords"]),
     close() {
       this.$emit("update", false);
+    },
+    deleteRecord(row) {
+      const arr = this.buySellRecords.filter(
+        (item, index) => row.index !== index
+      );
+      this._setBuySellRecords(arr);
     }
   }
 };
